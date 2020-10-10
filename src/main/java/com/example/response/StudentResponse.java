f04e35275f432d4039924d4733870205d6a7604e
SARAL <saxenasaral@gmail.com>
2020-10-10 19:11:04 +0530

    first commit for code

---
+++ src/main/java/com/example/response/StudentResponse.java
@@ -0,0 +1,49 @@
+package com.example.response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.entity.Student;
+import com.example.entity.Subject;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class StudentResponse {
+
+	private long id;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+	
+	private String street;
+
+	private String city;
+	
+	// learning subject is not exposed rite now
+	private List<SubjectResponse> learningSubjects;
+	
+	public StudentResponse (Student student) {
+		this.id = student.getId();
+		this.firstName = student.getFirstName();
+		this.lastName = student.getLastName();
+		this.email = student.getEmail();
+		
+		this.street = student.getAddress().getStreet();
+		this.city = student.getAddress().getCity();
+
+		// later we wil move the same to graphql resolver
+		if (student.getLearningSubjects() != null) {
+			learningSubjects = new ArrayList<SubjectResponse>();
+			for (Subject subject: student.getLearningSubjects()) {
+				learningSubjects.add(new SubjectResponse(subject));
+			}
+		}
+	}
+
+}
